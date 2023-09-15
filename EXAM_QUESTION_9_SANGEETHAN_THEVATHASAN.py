a1713647b6a944f6dd489b085536d84634b83534
sickboysang <144414737+sickboysang@users.noreply.github.com>
2023-09-14 23:24:26 -0400

    hi guys

---
+++ EXAM_QUESTION_9_SANGEETHAN_THEVATHASAN.py
@@ -0,0 +1,37 @@
+#Sangeethan Thevathasan 100867103 Question 9    
+word_to_number_1 = ['.',',','?','!',':']
+word_to_number_2 = ['A','B','C']
+word_to_number_3 = ['D','E','F']
+word_to_number_4 = ['G','H','I']
+word_to_number_5 = ['J','K','L']
+word_to_number_6 = ['M','N','O']
+word_to_number_7 = ['P','Q','R' 'S']
+word_to_number_8 = ['T','U','V']
+word_to_number_9 = ['W','X','Y' 'Z']
+word_to_number_10 = ['']
+
+word="hello World"
+list=[word_to_number_1,word_to_number_2,word_to_number_3,word_to_number_4,word_to_number_5,word_to_number_6,word_to_number_7,word_to_number_8,word_to_number_9,word_to_number_10]
+for word in list:
+    if word_to_number_1 in list:
+        print("1")
+    if word_to_number_2 in list:
+        print("2")
+    if word_to_number_3 in list:
+        print("3")
+    if word_to_number_4 in list:
+        print("4")
+    if word_to_number_5 in list:
+        print("5")
+    elif word_to_number_6 in list:
+        print("6")
+    if word_to_number_7 in list:
+        print("7")
+    if word_to_number_8 in list:
+        print("8")
+    if word_to_number_9 in list:
+        print("9")
+    if word_to_number_10 in list:
+        print("10")
+
+        
